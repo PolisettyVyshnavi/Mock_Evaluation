d9b57caa07384454abdb0ed918d0294d3ece60e8
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2025-12-28 20:24:52 +0530

    mini

--- Evaluation/question1/src/App.jsx
+++ Evaluation/question1/src/App.jsx
@@ -1,19 +1,19 @@
 import React,{ useState } from "react";
-import {BrowserRouter, Routes,Route,Navigate } from "react-router-dom";
+import { Routes,Route,Navigate } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import ProtectedRoute from "./ProtectedRoute";
 import AdminDashboard from "./AdminDashboard";
 function App(){
   const [isAuthenticated,setIsAuthenticated]=useState(false);
   return(
-   <BrowserRouter>
+   
       <Routes>
         <Route path='/login' element={<LoginPage setIsAuthenticated={setIsAuthenticated}/>}/>
         <Route path="/admin" element={
           <ProtectedRoute isAuthenticated={isAuthenticated}><AdminDashboard/></ProtectedRoute>}/>
        <Route path="*" element={<Navigate to="/login" replace/>}/>
       </Routes>
-    </BrowserRouter>
+   
   
   )
 }
